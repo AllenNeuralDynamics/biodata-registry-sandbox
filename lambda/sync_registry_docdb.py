b0da4f9567c55edf1e832973c809f884c6409a18
Jon Young <104453205+jtyoung84@users.noreply.github.com>
2026-04-24 13:47:24 -0700

    feat: adds lambda example

--- lambda/sync_registry_docdb.py
+++ lambda/sync_registry_docdb.py
@@ -1,10 +1,11 @@
 import json
 from typing import List, Dict
+from collections import defaultdict
 
 from kafka import KafkaConsumer
 import psycopg
 from psycopg.rows import dict_row
-from pymongo import MongoClient
+from pymongo import MongoClient, ReplaceOne
 from bson.binary import UuidRepresentation
 from bson.codec_options import CodecOptions
 import uuid
@@ -44,7 +45,7 @@ def flatten_data_asset_view_records(p_records: List[Dict]):
         return None
 
 
-def handle_data_asset(k_message, d_collection, p_conn):
+def handle_data_asset_change(k_message, d_collection, p_conn):
     k_data_asset_id = k_message.value["payload"]["after"].get("id")
     if k_data_asset_id is not None:
         with p_conn.cursor() as cur:
@@ -63,6 +64,146 @@ def handle_data_asset(k_message, d_collection, p_conn):
                 {"_id": hashed_uuid}, mongo_db_record, upsert=True
             )
 
+def handle_subject_change(k_message, d_collection, p_conn):
+    k_subject_id = k_message.value["payload"]["after"].get("id")
+    if k_subject_id is not None:
+        with p_conn.cursor() as cur:
+            p_record_groups = defaultdict(list)
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE subject_id = %s;",
+                (k_subject_id,)
+            )
+            for p_record in cur:
+                k_data_asset_id = p_record["data_asset_id"]
+                p_record_groups[k_data_asset_id].append(p_record)
+        replace_requests = []
+        for k_data_asset_id, p_records in p_record_groups.items():
+            mongo_db_record = flatten_data_asset_view_records(p_records)
+            if mongo_db_record is not None:
+                hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+                mongo_db_record["_id"] = hashed_uuid
+                replace_requests.append(
+                    ReplaceOne(
+                        {"_id": hashed_uuid},
+                        mongo_db_record,
+                        upsert=True
+                    )
+                )
+        if replace_requests:
+            d_collection.bulk_write(replace_requests)
+
+def handle_subject_procedure_change(k_message, d_collection, p_conn):
+    k_subject_id = k_message.value["payload"]["after"].get("subject_id")
+    if k_subject_id is not None:
+        with p_conn.cursor() as cur:
+            p_record_groups = defaultdict(list)
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE subject_id = %s;",
+                (k_subject_id,)
+            )
+            for p_record in cur:
+                k_data_asset_id = p_record["data_asset_id"]
+                p_record_groups[k_data_asset_id].append(p_record)
+        replace_requests = []
+        for k_data_asset_id, p_records in p_record_groups.items():
+            mongo_db_record = flatten_data_asset_view_records(p_records)
+            if mongo_db_record is not None:
+                hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+                mongo_db_record["_id"] = hashed_uuid
+                replace_requests.append(
+                    ReplaceOne(
+                        {"_id": hashed_uuid},
+                        mongo_db_record,
+                        upsert=True
+                    )
+                )
+        if replace_requests:
+            d_collection.bulk_write(replace_requests)
+
+def handle_instrument_change(k_message, d_collection, p_conn):
+    k_instrument_id = k_message.value["payload"]["after"].get("instrument_id")
+    if k_instrument_id is not None:
+        with p_conn.cursor() as cur:
+            p_record_groups = defaultdict(list)
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE k_instrument_id = %s;",
+                (k_instrument_id,)
+            )
+            for p_record in cur:
+                k_data_asset_id = p_record["data_asset_id"]
+                p_record_groups[k_data_asset_id].append(p_record)
+        replace_requests = []
+        for k_data_asset_id, p_records in p_record_groups.items():
+            mongo_db_record = flatten_data_asset_view_records(p_records)
+            if mongo_db_record is not None:
+                hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+                mongo_db_record["_id"] = hashed_uuid
+                replace_requests.append(
+                    ReplaceOne(
+                        {"_id": hashed_uuid},
+                        mongo_db_record,
+                        upsert=True
+                    )
+                )
+        if replace_requests:
+            d_collection.bulk_write(replace_requests)
+
+def handle_acquisition_change(k_message, d_collection, p_conn):
+    k_data_asset_id = k_message.value["payload"]["after"].get("data_asset_id")
+    if k_data_asset_id is not None:
+        with p_conn.cursor() as cur:
+            p_records = []
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE data_asset_id = %s;",
+                (k_data_asset_id,)
+            )
+            for p_record in cur:
+                p_records.append(p_record)
+        mongo_db_record = flatten_data_asset_view_records(p_records)
+        if mongo_db_record is not None:
+            hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+            mongo_db_record["_id"] = hashed_uuid
+            d_collection.replace_one(
+                {"_id": hashed_uuid}, mongo_db_record, upsert=True
+            )
+
+def handle_processes_change(k_message, d_collection, p_conn):
+    k_data_asset_id = k_message.value["payload"]["after"].get("output_data_asset_id")
+    if k_data_asset_id is not None:
+        with p_conn.cursor() as cur:
+            p_records = []
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE data_asset_id = %s;",
+                (k_data_asset_id,)
+            )
+            for p_record in cur:
+                p_records.append(p_record)
+        mongo_db_record = flatten_data_asset_view_records(p_records)
+        if mongo_db_record is not None:
+            hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+            mongo_db_record["_id"] = hashed_uuid
+            d_collection.replace_one(
+                {"_id": hashed_uuid}, mongo_db_record, upsert=True
+            )
+
+def handle_quality_controls_change(k_message, d_collection, p_conn):
+    k_data_asset_id = k_message.value["payload"]["after"].get("data_asset_id")
+    if k_data_asset_id is not None:
+        with p_conn.cursor() as cur:
+            p_records = []
+            cur.execute(
+                "SELECT * FROM data_asset_view WHERE data_asset_id = %s;",
+                (k_data_asset_id,)
+            )
+            for p_record in cur:
+                p_records.append(p_record)
+        mongo_db_record = flatten_data_asset_view_records(p_records)
+        if mongo_db_record is not None:
+            hashed_uuid = uuid.uuid5(namespace, str(k_data_asset_id))
+            mongo_db_record["_id"] = hashed_uuid
+            d_collection.replace_one(
+                {"_id": hashed_uuid}, mongo_db_record, upsert=True
+            )
 
 try:
     pg_conn_info = "host=localhost port=5432 dbname=registry user=user password=password"
@@ -82,23 +223,23 @@ try:
         for message in consumer:
             match message.topic:
                 case "registry.public.data_assets":
-                    handle_data_asset(message, collection, conn)
+                    handle_data_asset_change(message, collection, conn)
                 case "registry.public.subjects":
-                    print(message.topic)
+                    handle_subject_change(message, collection, conn)
                 case "registry.public.specimens":
                     print(message.topic)
                 case "registry.specimen_procedures":
                     print(message.topic)
                 case "registry.public.subject_procedures":
-                    print(message.topic)
+                    handle_subject_procedure_change(message, collection, conn)
                 case "registry.public.instruments":
-                    print(message.topic)
+                    handle_instrument_change(message, collection, conn)
                 case "registry.public.acquisitions":
-                    print(message.topic)
+                    handle_acquisition_change(message, collection, conn)
                 case "registry.public.quality_controls":
-                    print(message.topic)
+                    handle_quality_controls_change(message, collection, conn)
                 case "registry.public.processes":
-                    print(message.topic)
+                    handle_processes_change(message, collection, conn)
                 case _:
                     print("Unknown topic")
 except KeyboardInterrupt:
